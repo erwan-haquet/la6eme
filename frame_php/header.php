85b663ad17751d28bc6f419f90c92688bce0faa8
ErwanHaquet <ehaquet.dev@gmail.com>
2015-09-02 18:04:27 +0200

    Ajout de .SCSS, + modifications header/nav

---
+++ frame_php/header.php
@@ -0,0 +1,18 @@
+		<header>
+				<img src="http://www.collegejeanmonnet.com/spip/IMG/jpg/pic00105.jpg" alt="" width="161" height="121"/>
+				<h1>
+				La rentree en 6eme - College Jean-Monnet
+				</h1>
+		</header>
+
+		<nav>
+			<ul> 
+      			<li><a class="navigation-lien" href="#" title="titre">Accueil</a></li> 
+     			<li><a class="navigation-lien" href="#" title="titre">La vie au collège</a></li> 
+   			    <li><a class="navigation-lien" href="#" title="titre">Gallerie photos</a></li> 
+  			    <li><a class="navigation-lien" href="#" title="titre">FAQ</a></li> 
+  			    <li><a class="navigation-lien" href="#" title="titre">Liens utiles</a></li> 
+  			    <li><a class="navigation-lien" href="#" title="titre">Contact</a></li> 
+  			    <li style="float:none;"><br style="clear:both;" /></li> 
+  			</ul> 
+		</nav>
